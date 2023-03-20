81ce25f28ef79442e04c1e2f6bd305230bae1fc5
Ckopoer <1456242110@qq.com>
2023-03-20 22:15:50 +0800

    Assemble untested

--- spider.py
+++ spider.py
@@ -9,11 +9,8 @@ from multiprocessing import Pool
 import glob
 import os
 
-url2 = 'https://www.zxcs.info/content/plugins/cgz_xinqing/cgz_xinqing_action.php?action=mood&id={code}'
-# url3 = 'https://www.zxcs.info/download.php?id={code}'
 review = ['仙草', '粮草', '干草', '枯草', '毒草']
 ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36'
-host = 'www.zxcs.info'
 re1 = re.compile(r'^《(.*?)》.*?作者[：:] ?(.*?)$')
 re2 = re.compile(r'.*?(\d+)$')
 re3 = re.compile(r'.*?(\d+[.]\d+)')
@@ -73,7 +70,9 @@ def parse_tree(tree):
     return all_items
 
 
-def get_review(item, code):
+def get_review(item, code, host):
+    head = 'https://' if len(host) > 7 else 'http://'
+    url2 = head + host + '/content/plugins/cgz_xinqing/cgz_xinqing_action.php?action=mood&id={code}'
     logger.info('尝试获取' + item['title'] + '的评价数据')
     html = s.get(url2.format(code=code), timeout=30, headers={'User-Agent': ua, 'host':host})
     if not html:
@@ -84,17 +83,21 @@ def get_review(item, code):
     for i in range(len(temp)):
         item[review[i]] = temp[i]
 
-# def get_downloadLink(item, code):
-#     logger.info('尝试获取' + item['title'] + '的下载链接')
-#     html = s.get(url3.format(code=code), timeout=30, headers={'User-Agent': ua, 'host':host})
-#     try:
-#         html = fromstring(html.text)
-#         link = html.xpath('//span[@class="downfile"]/a')
-#         item['link1'] = link[0].xpath('@href')[0]
-#         item['link2'] = link[1].xpath('@href')[0]
-#     except:
-#         item['link1'] = ''
-#         item['link2'] = ''
+def get_downloadLink(item, code, host):
+    head = 'https://' if len(host) > 7 else 'http://'
+
+    url3 = head + host + '/download.php?id={code}'
+
+    logger.info('尝试获取' + item['title'] + '的下载链接')
+    html = s.get(url3.format(code=code), timeout=30, headers={'User-Agent': ua, 'host':host})
+    try:
+        html = fromstring(html.text)
+        link = html.xpath('//span[@class="downfile"]/a')
+        item['link1'] = link[0].xpath('@href')[0]
+        item['link2'] = link[1].xpath('@href')[0]
+    except:
+        item['link1'] = ''
+        item['link2'] = ''
     
     
     
@@ -111,7 +114,7 @@ def has_next(tree):
 
 
 
-def main(url, round=1000):
+def main(url, host, round=1000):
     urls = [url]
     novels = []
     while urls and round > 0:
@@ -123,8 +126,8 @@ def main(url, round=1000):
         tree = fromstring(html.text)
         items = parse_tree(tree)
         for item in items:
-            get_review(item, item['code'])
-            # get_downloadLink(item, item['code'])
+            get_review(item, item['code'], host)
+            # get_downloadLink(item, item['code'], host)
         novels.append(items)
         print(items)
         next_u = has_next(tree)
@@ -135,6 +138,8 @@ def main(url, round=1000):
     return novels
 
 def to_csv(novels, filename):
+    if not novels:
+        return
     data = pd.concat(map(pd.DataFrame, novels))
     data.to_csv(filename, index=False, encoding="utf_8_sig")
 
@@ -147,12 +152,21 @@ def mkdir(path):
     else:
         print( "---  There is this folder!  ---")
 
-def get_csv(url, filename):
-    novels = main(url)
+def get_csv(url, filename, host):
+    novels = main(url, host)
     to_csv(novels, filename)
 
 if __name__ == '__main__':
-    urls = {
+    urlsForMe = {
+            'http://zxcs.me/sort/23': '都市娱乐.csv', 
+            'http://zxcs.me/sort/25': '武侠仙侠.csv', 
+            'http://zxcs.me/sort/26': '奇幻玄幻.csv', 
+            'http://zxcs.me/sort/27': '科幻灵异.csv', 
+            'http://zxcs.me/sort/28': '历史军事.csv',
+            'http://zxcs.me/sort/29': '竞技游戏.csv',
+            'http://zxcs.me/sort/55': '二次元.csv'
+            }
+    urlsForInfo = {
             'https://www.zxcs.info/sort/3': '都市娱乐.csv', 
             'https://www.zxcs.info/sort/4': '武侠仙侠.csv', 
             'https://www.zxcs.info/sort/8': '奇幻玄幻.csv', 
@@ -163,27 +177,40 @@ if __name__ == '__main__':
             }
 
     Time = time.strftime("%Y-%m-%d", time.localtime())
-    path = "./result/Info/" + Time + "/"
-    mkdir(path)
-
-    # Test Code
-    # for url, filename in urls.items():
-    #     get_csv(url, path + filename)
+    path1 = "./result/" + Time + "/Me/"
+    path2 = "./result/" + Time + "/Info/"
+    mkdir(path1)
+    mkdir(path2)
 
 
-    p = Pool(len(urls))
-    for url, filename in urls.items():
-        p.apply_async(get_csv, args=(url, path + filename))
+    # p = Pool(len(urlsForMe) + len(urlsForInfo))
+    # for url, filename in urlsForMe.items():
+    #     p.apply_async(get_csv, args=(url, path1 + filename, 'zxcs.me'))
 
-    print('Waiting for all subprocesses done...')
-    p.close()
-    p.join()
-    print('All subprocesses done.')
+    # for url, filename in urlsForInfo.items():
+    #     p.apply_async(get_csv, args=(url, path2 + filename, 'www.zxcs.info'))
+
+    # print('Waiting for all subprocesses done...')
+    # p.close()
+    # p.join()
+    # print('All subprocesses done.')
 
 
-    all_filenames = [i for i in glob.glob(path + '*.csv')]
+    for url, filename in urlsForInfo.items():
+        get_csv(url, path2 + filename, 'www.zxcs.info')
+
+    print('Start Merge zxcs.me.')
+    all_filenames = [i for i in glob.glob(path1 + '*.csv')]
     #在列表中合并所有文件
     combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames])
     #导出 csv
-    combined_csv.to_csv(path + "Book.csv", index=False, encoding='utf-8-sig')
-    combined_csv.to_excel(path + 'Book.xlsx', index=False, sheet_name='data', encoding='utf-8-sig')
+    combined_csv.to_csv(path1 + "Book.csv", index=False, encoding='utf-8-sig')
+    combined_csv.to_excel(path1 + 'Book.xlsx', index=False, sheet_name='data', encoding='utf-8-sig')
+
+    print('Start Merge zxcs.info.')
+    all_filenames = [i for i in glob.glob(path2 + '*.csv')]
+    #在列表中合并所有文件
+    combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames])
+    #导出 csv
+    combined_csv.to_csv(path2 + "Book.csv", index=False, encoding='utf-8-sig')
+    combined_csv.to_excel(path2 + 'Book.xlsx', index=False, sheet_name='data', encoding='utf-8-sig')
